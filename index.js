b3172186f68e19b59f87e81b07e3b5996f8df0d5
Kartik Upadhyay <kartikupadhyay200@gmail.com>
2026-03-19 15:34:57 +0530

    javascript changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Spotify Clone");
 async function getSongs(){
-    let a = await fetch("http://127.0.0.1:5500/assets/")
+    let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
@@ -10,10 +10,19 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
-console.log(getSongs());
-getSongs()
+async function main() {
+    let songs = await getSongs()
+    console.log(songs);
+    let songUL = document.querySelector(".song-list").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML += `<li>${song.replaceAll("%20", " ")}</li>`
+    }
+    // let audio = new Audio(songs[0])
+    // audio.play()
+}
+main()
