7f87083c473c49747ea98cd78d4c01ad1262484c
Kartik Upadhyay <kartikupadhyay200@gmail.com>
2026-03-19 22:57:00 +0530

    javascript sidebar changes

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-console.log("Spotify Clone");
-async function getSongs(){
+// console.log("Spotify Clone");
+let currentSong = new Audio()
+async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
     // console.log(response);
     let as = div.getElementsByTagName("a")
-    let songs= []
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
+const playMusic = (track)=>{
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+}
 async function main() {
     let songs = await getSongs()
-    console.log(songs);
+    // console.log(songs);
+    // let audio = new Audio("http://127.0.0.1:5500/songs/" + songs[0])
+    // audio.play()
+
     let songUL = document.querySelector(".song-list").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML += `<li>${song.replaceAll("%20", " ")}</li>`
+        const songName = song.replaceAll("%20", " ").replaceAll("%5BFrom Ranabaali%5D", "").replace(".mp3", "").replaceAll("(pagalall.com)", "")
+        songUL.innerHTML += `<li data-track="${song}">
+            <img src="assets/music.svg" alt="music" class="song-icon">
+            <div class="Info">
+                <p class="song-name">${songName}</p>
+                <p class="artist-name">KARTIK</p>
+            </div>
+            <div class="playicon">
+                <img src="assets/play.svg" alt="play" width="20">
+            </div>
+        </li>`  
     }
-    // let audio = new Audio(songs[0])
-    // audio.play()
+    // Adding event listeners to play icons
+    Array.from(document.querySelector(".song-list").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",()=>{
+            const track = e.getAttribute("data-track")
+            console.log("Playing:", track.replaceAll("%20"," ").replace(".mp3", ""))
+            playMusic(track)
+        })
+    })
 }
 main()
